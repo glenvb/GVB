041ddb459abb24484c3eb7531a1db82f401eb905
GVB <glenvanblaricum@gmail.com>
2017-02-21 17:29:29 +1100

    hello

---
+++ hello/hello.cpp
@@ -0,0 +1,13 @@
+// hello world.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
